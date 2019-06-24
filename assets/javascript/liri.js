a6ab3672cf1ab973390def4ddba4e33c053fe80a
Juwami <sean.nguyenf@yahoo.com>
2019-06-23 21:53:51 -0400

    added bandsintown api

--- assets/javascript/liri.js
+++ assets/javascript/liri.js
@@ -1,5 +1,39 @@
 require('dotenv').config()
 
+let axios = require('axios')
 let keys = require('./keys.js')
 
-let spotify = new spotify(keys.spotify)
+// let spotify = new Spotify(keys.spotify)
+
+let request = process.argv[3]
+
+switch (process.argv[2]) {
+    case 'concert-this':
+        console.log('concert')
+        concertThis(process.argv[2])
+        break
+
+    case 'spotify-this-song':
+        console.log('spotify')
+        break
+
+    case 'movie-this':
+        console.log('movie')
+        break
+
+    case 'do-what-it-says':
+        console.log('do')
+        break
+}
+
+// case 'concert-this' function
+function concertThis (artist) {
+    console.log(artist)
+    axios
+        .get('https://rest.bandsintown.com/artists/' + artist + '/events?app_id=codingbootcamp')
+        .then(function (response) {
+            console.log(response)
+        })
+}
+
+// case 'spotify-this-song' function
