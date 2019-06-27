25b38b92ff834031a00a25db51fcd575cdb26a0a
Juwami <sean.nguyenf@yahoo.com>
2019-06-26 21:30:06 -0400

    spotifyThis() function completed and pulling API data using for loop

--- liri.js
+++ liri.js
@@ -70,7 +70,19 @@ function spotifyThis(song) {
             if (err) {
                 return console.log('Error occured: ', err)
             }
-            console.log(data.tracks)
+            // Artist(s)
+            // The song 's name
+            // A preview link of the song from Spotify
+            // The album that the song is from
+            let results = data.tracks.items
+            // console.log(results[0])
+            for (i = 0; i < results.length; i++) {
+                console.log(`Artist(s): ${results[i].artists[0].name}`)
+                console.log(`Song's Name: ${results[i].name}`)
+                console.log(`Spotify Song Link: ${results[i].external_urls.spotify}`)
+                console.log(`Album Name: ${results[i].album.name}`)
+                console.log('----------------------------------------- \n')
+            }
         })
 }
 
